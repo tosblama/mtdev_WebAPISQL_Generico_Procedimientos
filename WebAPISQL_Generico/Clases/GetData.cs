c328ce5029b26e46fe496169fc53b2da4d8a4813
ElProgramadorVago <ElProgramadorVago@W11-EPV>
2023-01-22 19:29:26 +0100

    Agregar archivos de proyecto.

---
+++ WebAPISQL_Generico/Clases/GetData.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace WebAPISQL_Generico.Clases
+{
+    public class GetData
+    {
+        public async Task<string> GetDbData(string nombre, string parametros = "")
+        {
+            // Buscar consulta asociada al nombre recibido
+            DataTable dt = AccesoDatos.GetTmpDataTable($"SELECT * FROM WebAPIConsultas WHERE Nombre='{nombre}'");
+            if (dt.Rows.Count == 0) { return "NotFound"; }
+            string query = dt.Rows[0]["Consulta"].ToString();
+
+            // Si hay parámetros, asignarlos (separamos los parámetros por el caracter |)
+            if (parametros.Length > 0)
+            {
+                string[] listaParametros = parametros.Split('|');
+                for (int i = 0; i < listaParametros.Length; i++)
+                {
+                    query = query.Replace("@p" + (i + 1).ToString(), listaParametros[i]);
+                }
+            }
+
+            // Ejecutar consulta
+            return await AccesoDatos.JsonDataReader(query);
+        }
+
+    }
+}
---
+++ WebAPISQL_Generico/Controllers/GetDataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPISQL_Generico.Controllers
+{
+    public class GetDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("getDbData")]
+        public async Task<ActionResult> GetDbData(string nombre, string parametros = "")
+        {
+            Clases.GetData gd = new Clases.GetData();
+            string resultado = await gd.GetDbData(nombre, parametros);
+            if (resultado == "NotFound")
+            {
+                return NotFound($"El método {nombre} no se ha encontrado");
+            }
+            else
+            {
+                return Ok(resultado);
+            }
+        }
+    }
+}
